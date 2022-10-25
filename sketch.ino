f371c9dd992313881c127ffb95acfb2fd206ca26
Makar Ivashko <1212makar1212@gmail.com>
2022-10-25 23:40:15 +0300

    first commit

---
+++ sketch.ino
@@ -0,0 +1,14 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Hello world");
+}
+
+void loop() {
+  
+}
