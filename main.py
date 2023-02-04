9b947a1f62ca1657673510b6c46e568f22b93c84
Makar Ivashko <1212makar1212@gmail.com>
2023-02-04 17:35:22 +0200

    Script refactoring, added display

--- main.py
+++ main.py
@@ -1,28 +1,64 @@
 import matplotlib.pyplot as plt
-import csv
+import matplotlib.animation as animation
+import sys
 
-time_min_arr: list[float] = []
-temp1_arr: list[int] = []
-temp2_arr: list[int] = []
-with open('main.csv', 'r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        if len(row) == 3:
-            # print(len(row))
-            try:
-                time_sec: float = int(row[2])
-                temp1: int = int(row[0])
-                temp2: int = int(row[1])
-                time_min_arr.append(time_sec / 60)
-                temp1_arr.append(temp1)
-                temp2_arr.append(temp2)
-            except:
-                print('NaN ' + row[0] + ' ' + row[1])
+file_path = 'out.txt'
 
-plt.grid()
-plt.scatter(time_min_arr, temp1_arr, s=3)
-plt.scatter(time_min_arr, temp2_arr, s=3)
-plt.xlabel('Time')
-plt.ylabel('Temperature')
-plt.show()
+def process_line(line):
+    parts = line.strip().split(',')
+    numbers = []
+    for part in parts:
+        numbers.append(float(part))
+    return numbers
 
+def read_file():
+    time_values = []
+    temp1_values = []
+    temp2_values = []
+    last_time: int = 0
+    with open(file_path, 'r') as file:
+        for line in file:
+            if len(line) <= 1:
+                continue
+            values = line.split(' ', 1)
+            if len(values) != 2:
+                print(f"Line without data (not 2 values): {line}")
+                continue
+            first_part, rest_of_line = values[0], values[1]
+            if first_part != 'D':
+                print(f"Line without data (not 'D'): {line}")
+                continue
+            numbers = process_line(rest_of_line)
+            if numbers and len(numbers) != 3:
+                print(f"Unexpected number count ({len(numbers)}): {line}")
+                continue
+            last_time = numbers[2]
+            time_values.append(numbers[2] / 60)
+            temp1_values.append(numbers[0])
+            temp2_values.append(numbers[1])
+    time_str = "{:02d}:{:02d}".format(int(int(last_time) / 60), int(last_time) % 60)
+    temps_str = "Substrate: {:3.02f}°C, Source: {:3.02f}°C".format(temp1_values[-1], temp2_values[-1]);
+    title = f'Time: {time_str}, {temps_str}'
+    return time_values, temp1_values, temp2_values, title
+
+def update_graph(frame):
+    time_values, temp1_values, temp2_values, title = read_file()
+    plt.clf()
+    plt.grid()
+    plt.title(title, fontsize = 20)
+    plt.xlabel('Time, min')
+    plt.ylabel('Temperature, C')
+    plt.scatter(time_values, temp1_values, label='1', s=3)
+    plt.scatter(time_values, temp2_values, label='2', s=3)
+
+def main():
+    fig = plt.figure()
+    ani = animation.FuncAnimation(fig, update_graph, interval=1000)
+    plt.show()
+
+if __name__ == "__main__":
+    print(f"cmd entry: {sys.argv}")
+    if len(sys.argv) >= 2:
+        print(f"Overriding file path to: {sys.argv[1]}")
+        file_path = sys.argv[1]
+    main()
---
+++ upload_sketch/upload_sketch.ino
@@ -0,0 +1,89 @@
+#if 0
+0. Setup
+ls -la /dev/ttyACM0 && sudo chmod a+rw /dev/ttyACM0 && ls -la /dev/ttyACM0
+
+1. Read
+cat -v /dev/ttyACM0 | tee ~/Arduino/out.txt
+
+2. Make a graph:
+cd ~/Arduino && python3 main.py out.txt
+
+#endif
+
+#include <LiquidCrystal_I2C.h>
+#include <MAX6675_Thermocouple.h>
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+MAX6675_Thermocouple thermocouple1(4, 5, 6); // 2 - GND, 3 - 5V, SCK, CS, SO
+MAX6675_Thermocouple thermocouple2(10, 11, 12); // 8 - GND, 9 - 5V, SCK, CS, SO
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("\n\n\n\n\nSetup");
+    
+    // setup thermocouple1
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT); 
+    digitalWrite(2, LOW);
+    digitalWrite(3, HIGH);
+
+    // setup thermocouple2
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    digitalWrite(8, LOW);
+    digitalWrite(9, HIGH);
+
+    // setup LCD
+    lcd.init();
+    lcd.backlight();
+    lcd.print("Hello world");
+    
+    delay(500);
+}
+
+double c1, c2;
+char buf[255];
+
+unsigned long curTime = 0;
+unsigned long measureAtMsec = 1500;
+unsigned long screenUpdateInterval = 1000;
+// unsigned long serialUpdateInterval = 1000;
+
+// Display interaction (buffers and printing)
+constexpr size_t lineBufSize = 17;
+char lineBuf[lineBufSize];
+
+inline int ip(double d) {
+    return (int)d;
+}
+
+inline int fp(double d) {
+    return (int)((d - (int)d) * 100);
+} 
+
+void loop() {
+    if(measureAtMsec <= millis()) {
+        measureAtMsec += screenUpdateInterval;
+        curTime = millis() / 1000;
+        c1 = thermocouple1.readCelsius(); 
+        c2 = thermocouple2.readCelsius(); 
+        snprintf(buf, 255, "D %d.%02d,%d.%02d,%lu", ip(c1), fp(c1), ip(c2), fp(c2), curTime);
+        Serial.println(buf);
+
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        snprintf(lineBuf, lineBufSize, "Time %02lu:%02lu", curTime / 60LU, curTime % 60LU);
+        lcd.print(lineBuf);
+
+        lcd.setCursor(0, 1);
+        snprintf(lineBuf, lineBufSize, "T1: %3d.%02d", ip(c1), fp(c1));
+        lcd.print(lineBuf);
+
+        lcd.setCursor(0, 2);
+        snprintf(lineBuf, lineBufSize, "T2: %3d.%02d", ip(c2), fp(c2));
+        lcd.print(lineBuf);
+    }
+}
+
+
+
