f6509535490c2b2c7feb744c37830bf9937d7eeb
Makar Ivashko <1212makar1212@gmail.com>
2023-04-14 22:40:49 +0300

    Refactoring (not tested)

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,38 +1,13 @@
 #include <MAX6675_Thermocouple.h>
 
-// #include <LiquidCrystal_I2C.h>
-// LiquidCrystal_I2C lcd(0x27, 20, 4);
-
-// MAX6675_Thermocouple thermocouple2(4, 5, 6); // 2 - GND, 3 - 5V, SCK, CS, SO
-
-class Timer {
-public:
-    void start(unsigned long duration) {
-        mStartTime = millis();
-        mDuration = duration;
-    }
-
-    bool isExpired() {
-        if(mDuration + mStartTime >= millis())
-            return true;
-        return false;
-    } 
-
-private:
-    unsigned long mStartTime = 0;
-    unsigned long mDuration = 0;
-};
-
-char serialBuf[255];
-inline int ip(double d) { return (int)d; }
-inline int fp(double d) { return (int)((d - (int)d) * 100); } 
+char serialBuf[255]; 
+int ip(double d) { return (int)d; }
+int fp(double d) { return (int)((d - (int)d) * 100); }
 
 class Controller {
 public:
     Controller(int tcGND, int tc5V, int tcSCK, int tcCS, int tcSO, int irelayControlPin, int irelayGroundPin, int itargetTemp)
-    : tcGNDpin(tcGND), tc5Vpin(tc5V), thermocouple(tcSCK, tcCS, tcSO), relayControlPin(irelayControlPin), relayGroundPin(irelayGroundPin), targetTemp(itargetTemp) {
-        
-    }
+    : tcGNDpin(tcGND), tc5Vpin(tc5V), thermocouple(tcSCK, tcCS, tcSO), relayControlPin(irelayControlPin), relayGroundPin(irelayGroundPin), targetTemp(itargetTemp) {}
 
     void setup() {
         // setup relay
@@ -59,14 +34,14 @@ private:
     unsigned long currentTime = 0;
     int controlValue = 0; // [0..1000], represents how ms in seconds relay will be ON
     double previousError = 0;
-    int targetTemp = 100;
+    int targetTemp;
     double kp = 0.02, kd = 0.50;
 
 
     // Thermocouple
     static constexpr int NUM_TEMP_READS = 4;
-    static constexpr int TEMP_READ_INTERVAL = 250;
-    double readings[NUM_TEMP_READS + 2] = { 0 };
+    static constexpr int TEMP_READ_INTERVAL = 1000 / NUM_TEMP_READS;
+    double readings[NUM_TEMP_READS] = { 0 };
     int readingIndex = 0;
     unsigned long nextReadTime = 0;
     int tcGNDpin;
@@ -91,21 +66,11 @@ private:
         readings[readingIndex] = temp;
         readingIndex = (readingIndex + 1) % NUM_TEMP_READS;
 
-        // snprintf(serialBuf, 255, "Reading done, index=%d, temp=%d, %d.%2d", readingIndex, temp, ip(tempDouble), fp(tempDouble));
-        // Serial.println(serialBuf);
-
         if(readingIndex != 0)
             return;
 
         double averageTemp = calculateAverageTemp();
-
-
-
         controlValue = calculateControlValue(averageTemp);
-
-        // snprintf(serialBuf, 255, "%d readings done, avg temp=%d,%02d, control value = %d", NUM_TEMP_READS, averageTemp / 100, averageTemp % 100, controlValue);
-        // Serial.println(serialBuf);
-
         relayCycleStart = currentTime;
     }
 
@@ -122,23 +87,22 @@ private:
         double deriv = error - previousError;
         previousError = error;
         
-        double p = kp * double(error);
-        double d = kd * double(deriv);
-        double cv = p + d;
-        double r = cv;
+        double p = kp * error;
+        double d = kd * deriv;
+        double uncappedCV = p + d;
 
-        if (cv < 0.0) cv = 0.0;
-        if (cv > 0.0 && cv < 0.05) cv = 0.0;
-        if (cv > 1.0) cv = 1.0;
-        if (cv < 1.0 && cv > 0.95) cv = 1.0;
+        double cv = min(1.0, max(0.0, uncappedCV));
 
-        snprintf(serialBuf, 255, "Error=%d.%02d, P=%d D=%d RES=%d TRES=%d T=%d.%02d"
-            , ip(error), fp(error),int(p * 1000), int(d * 1000), int(r * 1000), int(cv * 1000), ip(temperature), fp(temperature));
-        Serial.println(serialBuf);
+        if (cv < 0.01) cv = 0.0;
+        if (cv > 0.99) cv = 1.0;
 
-        snprintf(serialBuf, 255, "LINE: %lu,%d.%02d,%d"
+        snprintf(serialBuf, 255, "TRACE: ERR=%3d P=%3d D=%3d uCV=%3d CV=%3d T=%3d"
+            , ip(error),int(p * 100), int(d * 100), int(uncappedCV * 100), int(cv * 100), ip(temperature), fp(temperature));
+        Serial.print(serialBuf);
+
+        snprintf(serialBuf, 255, "INFO: %lu,%d.%02d,%d"
             , currentTime / 1000, ip(temperature), fp(temperature), int(cv * 1000));
-        Serial.println(serialBuf);
+        Serial.print(serialBuf);
 
 
         return (int)(1000.0 * cv);
@@ -158,7 +122,7 @@ private:
             relayState = false;
         }
         else if (elapsedCycleTime >= 1000) {
-            Serial.println("Overshoot");
+            Serial.print("Overshoot\nOvershoot\nOvershoot\n");
             relayState = controlValue == 1000;
         }
 
@@ -171,14 +135,8 @@ Controller controllerSubstr1(2, 3, 4, 5, 6, 22, 23, 420);
 
 
 void setup() {
-    // setup serial
     Serial.begin(9600);
     Serial.println("\n\n\n\n\nSetup");
-
-    // setup LCD
-    // lcd.init();
-    // lcd.backlight();
-    // lcd.print("Hello world");
 
     controllerSubstr1.setup();
 }
@@ -187,65 +145,15 @@ void loop() {
     controllerSubstr1.loop();
 }
 
-#if 0
-
-constexpr size_t lineBufSize = 17;
-char lineBuf[lineBufSize];
-char serialBuf[255];
-inline int ip(double d) { return (int)d; }
-inline int fp(double d) { return (int)((d - (int)d) * 100); } 
-void pollOutput() {
-    // LCD
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    snprintf(lineBuf, lineBufSize, "Time %02lu:%02lu", currentTime / 60LU, currentTime % 60LU);
-    lcd.print(lineBuf);
-    lcd.setCursor(0, 1);
-    snprintf(lineBuf, lineBufSize, "T1: %3d.%02d", ip(temp1), fp(temp1));
-    lcd.print(lineBuf);
-    lcd.setCursor(0, 2);
-    snprintf(lineBuf, lineBufSize, "T2: %3d.%02d", ip(temp2), fp(temp2));
-    lcd.print(lineBuf);
-
-    // Serial
-    currentTime = millis() / 1000;
-    snprintf(serialBuf, 255, "D T1=%d.%02d,T2=%d.%02d,time=%lu,control=%u P=%d D=%d, error=%d", 
-            ip(temp1), fp(temp1), ip(temp2), fp(temp2), currentTime, controlValue / 10, int(kp * error * 100.0), int(kd * deriv * 100.0), int(error));
-    Serial.println(serialBuf);
-}
 
 
-unsigned long pollTime = 0;
-void loop() {
-    pollRelay(); // <- fast switching, no delays here
-    
-    if(pollTime <= millis()) {
-        pollTime += 1000;
-        pollInput();
-        pollLogic();
-        pollOutput();
-    }
-}
 
+// #include <LiquidCrystal_I2C.h>
+// LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-#endif
-
-#if 0
-char buf[255];
-
-unsigned long curTime = 0;
-unsigned long measureAtMsec = 1500;
-unsigned long screenUpdateInterval = 1000;
-
-// Display interaction (buffers and printing)
-constexpr size_t lineBufSize = 17;
-char lineBuf[lineBufSize];
-
-
-unsigned long switchAtMsec = 1000;
-unsigned long switchUpdateInterval = 5000;
-bool switchState = 0;
-#endif
-
+// setup LCD
+// lcd.init();
+// lcd.backlight();
+// lcd.print("Hello world");
 
 
