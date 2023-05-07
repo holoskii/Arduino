9548335e9fa9e67b299fb7d0ae6e642386a9617f
Makar Ivashko <1212makar1212@gmail.com>
2023-05-07 12:36:49 +0300

    Refactored script, improved performance

--- main.py
+++ main.py
@@ -5,44 +5,46 @@ import time
 
 file_path = 'out.txt'
 
-def process_line(line):
-    parts = line.strip().split(',')
-    numbers = []
-    for part in parts:
-        numbers.append(float(part))
-    return numbers
-
 def read_file():
-    time_values = []
-    temp1_values = []
-    control1_values = []
+    time_values:     list[float] = []
+    temp1_values:    list[float] = []
+    control1_values: list[float] = []
+    temp2_values:    list[float] = []
+    control2_values: list[float] = []
 
-    temp2_values = []
-    control2_values = []
-    last_time: int = 0
     with open(file_path, 'r') as file:
         for line in file:
-            if len(line) <= 1:
+            # Check that the line looks valid
+            if len(line) <= 1 or not line.startswith("INFO"):
                 continue
-            values = line.split(' ', 1)
-            if len(values) != 2:
-                print(f"Line without data (not 2 values): {line}")
+
+            # Verify that the line contains data
+            delimiter_index = line.find(' ')
+            if delimiter_index == -1:
+                print(f"Line without data (no space): {line}")
                 continue
-            first_part, rest_of_line = values[0], values[1]
-            if first_part != 'INFO:':
-                continue
-            numbers = process_line(rest_of_line)
-            if numbers and len(numbers) != 5:
+
+            # Extract numbers from the line
+            numbers: list[float] = []
+            for part in line[delimiter_index + 1:].split(','):
+                numbers.append(float(part))
+
+            # Verify again
+            if len(numbers) != 5:
                 print(f"Unexpected number count ({len(numbers)}): {line}")
                 continue
+
+            # Assign data to corresponding lists
             time_values.append(numbers[0] / 60)
             temp1_values.append(numbers[1])
             control1_values.append(numbers[2])
             temp2_values.append(numbers[3])
             control2_values.append(numbers[4])
+    
+    # Build title
     temp1 = temp1_values[-1] if len(temp1_values) > 0 else 0
     temp2 = temp2_values[-1] if len(temp2_values) > 0 else 0
-    temps_str = "Substrate(blue): {:3.01f}°C, Source(red): {:3.01f}°C".format(temp1, temp2);
+    temps_str = "Substrate B: {:3.01f}°C, Source R: {:3.01f}°C".format(temp1, temp2);
     title = f'{temps_str}'
     return time_values, temp1_values, control1_values, temp2_values, control2_values, title
 
@@ -52,16 +54,18 @@ def update_graph(frame):
     time_values, temp1_values, control1_values, temp2_values, control2_values, title = read_file()
 
     end = time.time()
-    print("Time " + str(end - start))
+    print("File parsing time = {:.0f} ms".format(1000 * (end - start)))
     plt.clf()
     plt.grid()
     plt.title(title, fontsize = 20, y=1.04)
     plt.xlabel('Time, min', fontsize=20)
     plt.ylabel('Temperature, C', fontsize=20)
-    plt.scatter(time_values, temp1_values, label='1', s=3, color = 'b')
-    plt.scatter(time_values, control1_values, label='2', s=3, color = 'b')
-    plt.scatter(time_values, temp2_values, label='3', s=3, color = 'r')
-    plt.scatter(time_values, control2_values, label='4', s=3, color = 'r')
+    plt.scatter(time_values, temp1_values, label='Substrate', s=3, color = 'b')
+    plt.scatter(time_values, control1_values, s=3, color = 'b')
+    plt.scatter(time_values, temp2_values, label='Source', s=3, color = 'r')
+    plt.scatter(time_values, control2_values, s=3, color = 'r')
+    plt.legend()
+
 
 def main():
     fig = plt.figure()
