41f7659ad5dd387460492b843693e50bf052fb4b
Makar Ivashko <1212makar1212@gmail.com>
2023-07-20 00:06:10 +0300

    Mostly refactoring, almost no functional changes

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,4 +1,5 @@
 #include <MAX6675_Thermocouple.h>
+#include <assert.h>
 
 // ############################################## USER SET VARIABLES
 //                          SUBSTRATE
@@ -15,11 +16,7 @@ static constexpr int        SOURCE_TEMP = 430;
 static constexpr double     SOURCE_KP   = 0.02;
 static constexpr double     SOURCE_KD   = 0.40;
 
-// COMMON
-static constexpr long       DEPOSITION_TIME_MS = 10 * 60 * 1000;
-static constexpr bool       DEBUG_ENABLED = true;
 // ############################################## USER SET VARIABLES
-
 
 inline int ip(double d) { return (int)d; }
 inline int fp(double d) { return (int)((d - (int)d) * 100); }
@@ -28,9 +25,12 @@ static constexpr int NUM_TEMP_READS = 4;
 static constexpr int TEMP_READ_INTERVAL = 1000 / NUM_TEMP_READS;
 
 unsigned long currentTime     = 0;  // For more predictable logic calculations, use one time for each loop call
-bool          depositionEnded = false;
 
-// Controls PID, relays and thermocouples. 2 for reactor, one for both source and substrate
+/**
+ * @class Controller
+ * @brief This class represents a controller for PID, relays, and thermocouples.
+ * A Reactor object has two controllers, one for the source and one for the substrate.
+ */
 class Controller {
 public:
     Controller(
@@ -41,6 +41,11 @@ public:
     , relayControlPin(relayControlPin), relayGroundPin(relayGroundPin), powerLimit(powerLimit)
     , targetTemp(targetTemp), tempOffet(tempOffet), kp(kp), kd(kd) {}
 
+    // #################### FUNCTIONS ####################
+
+    /**
+     * @brief Set up the pins for the controller
+     */
     void setup() {
         // setup relay
         pinMode(relayControlPin, OUTPUT);
@@ -55,26 +60,10 @@ public:
         digitalWrite(tc5Vpin, HIGH);
     }
 
-    // Logic
-    int controlValue = 0; // [0..1000], represents how ms in seconds relay will be ON
-    double previousError = 0;
-    const double powerLimit;
-    const int targetTemp;
-    const double tempOffet;
-    const double kp, kd;
-
-    // Thermocouple
-    double readings[NUM_TEMP_READS] = { 0 };
-    int readingIndex = 0;
-    const int tcGNDpin;
-    const int tc5Vpin;
-    const MAX6675_Thermocouple thermocouple;
-
-    // Relay
-    const int relayControlPin;
-    const int relayGroundPin;
-    bool relayState = false;
-
+    /**
+     * @brief Poll logic state and calculate control value
+     * @return True if logic was recalculated, otherwise false
+     */
     bool pollLogic() {
         // Read temperature multiple times and find average to reduce noise
         const double temp = thermocouple.readCelsius(); 
@@ -97,8 +86,26 @@ public:
         return sum / NUM_TEMP_READS;
     }
 
-    double temperature = 0, error = 0, deriv = 0, p = 0, d = 0, uncappedCV = 0, cv = 0;
+    /**
+     * @brief Poll the state of the relay
+     * @param elapsedCycleTime The elapsed cycle time
+     */
+    void pollRelay(unsigned long elapsedCycleTime) {
+        relayState = (elapsedCycleTime < controlValue);
 
+        if (elapsedCycleTime >= 1000) {
+            Serial.print("Overshoot\nOvershoot\nOvershoot\n");
+            relayState = controlValue == 1000;
+        }
+
+        digitalWrite(relayControlPin, relayState ? HIGH : LOW);
+    }
+
+private:
+    /**
+     * @brief Calculate the average temperature from the stored readings
+     * @return The average temperature
+     */
     int calculateControlValue(double inTemperature) {
         // Save all these intermediate values to trace them
         temperature = inTemperature;
@@ -110,9 +117,6 @@ public:
         d = kd * deriv;
         uncappedCV = p + d;
 
-        if (depositionEnded)
-            return 0;
-
         cv = min(powerLimit, max(0.0, uncappedCV));
 
         // Switch time is 10 ms, so avoid any time intervals < 10ms
@@ -122,39 +126,72 @@ public:
         return (int)(1000.0 * cv);
     }
 
-    void pollRelay(unsigned long elapsedCycleTime) {
-        relayState = (elapsedCycleTime < controlValue);
+    // #################### VARIABLES ####################
+public:
+    // Intermediate variables, used for monitoring
+    double temperature = 0;
+    double error = 0;
+    double deriv = 0;
+    double p = 0;
+    double d = 0;
+    double uncappedCV = 0;
+    double cv = 0;
 
-        if (elapsedCycleTime >= 1000) {
-            Serial.print("Overshoot\nOvershoot\nOvershoot\n");
-            relayState = controlValue == 1000;
-        }
+private:
+    // Logic
+    int controlValue = 0; // [0..1000], represents how ms in seconds relay will be ON
+    double previousError = 0;
+    const double powerLimit;
+    const int targetTemp;
+    const double tempOffet;
+    const double kp, kd;
 
-        digitalWrite(relayControlPin, relayState ? HIGH : LOW);
-    }
+    // Thermocouple
+    double readings[NUM_TEMP_READS] = { 0 };
+    int readingIndex = 0;
+    const int tcGNDpin;
+    const int tc5Vpin;
+    const MAX6675_Thermocouple thermocouple;
+
+    // Relay
+    const int relayControlPin;
+    const int relayGroundPin;
+    bool relayState = false;
 };
 
-// Manages 2 controllers, sends data through serial
+/**
+ * @class Reactor
+ * @brief This class represents a real reactor setup. 
+ * It manages two separate heaters: one for the substrate and one for the source.
+ */
 class Reactor {
 public:
-    static const size_t serialBufLen = 1024;
-    char serialBuf[serialBufLen];
-
-    Controller substrateController {2, 3,  4,  5,  6, 22, 23, SUBSTRATE_POWER_LIMIT, SUBSTRATE_TEMP, SUBSTRATE_TEMP_OFFSET, SUBSTRATE_KP, SUBSTRATE_KD}; // Substrate
-    Controller sourceController    {8, 9, 10, 11, 12, 24, 25, SOURCE_POWER_LIMIT,    SOURCE_TEMP,    SOURCE_TEMP_OFFSET,    SOURCE_KP,    SOURCE_KD   }; // Source
-
-    unsigned long relayPollTime = 0;
-    unsigned long nextReadTime = 0;
-    unsigned long relayCycleStart = 0;
-
-    bool depositionStarted = false;
-    unsigned long depositionStartTime = 0;
-
+    /**
+     * @brief Setup the reactor
+     */
     void setup() {
+        sendParameters();
         substrateController.setup();
         sourceController.setup();
     }
 
+    /**
+     * @brief Send user parameters of the reactor setup to the serial port
+     */
+    void sendParameters() {
+        int pos = snprintf(serialBuf, serialBufLen
+            , "START: SUBSTR: T=%3d TO=%3d KP=%d.%02d KD=%d.%02d; "
+                "SUBSTR: T=%3d TO=%3d KP=%d.%02d KD=%d.%02d\n\n"
+            , SUBSTRATE_TEMP, SUBSTRATE_TEMP_OFFSET, ip(SUBSTRATE_KP), fp(SUBSTRATE_KP), ip(SUBSTRATE_KD), fp(SUBSTRATE_KD)
+            , SOURCE_TEMP, SOURCE_TEMP_OFFSET, ip(SOURCE_KP), fp(SOURCE_KP), ip(SOURCE_KD), fp(SOURCE_KD));
+        assert(pos < serialBufLen);
+        Serial.write(serialBuf, pos);
+        Serial.flush(); 
+    }
+
+    /**
+     * @brief Main reactor loop. Limits polling to once a millisecond
+     */
     void loop() {
         currentTime = millis();
 
@@ -164,51 +201,10 @@ public:
             substrateController.pollLogic();
             bool calcLogic = sourceController.pollLogic();
 
-            // Print only if logic was recalculated
+            // Send data only if logic was recalculated
             if(calcLogic) {
                 relayCycleStart = currentTime;
-
-                // if (!depositionStarted) {
-                //     if (sourceController.temperature >= sourceController.targetTemp * 0.99 && abs(sourceController.deriv) < 50) {
-                //         depositionStarted = true;
-                //         depositionStartTime = currentTime;
-                //     }
-                // }
-                // else {
-                //     if (currentTime - depositionStartTime > DEPOSITION_TIME_MS) {
-                //         depositionEnded = true;
-                //     }
-                // }
-
-                int pos = snprintf(serialBuf, serialBufLen
-                    , "INFO: %lu,"
-                      "%d.%02d,%d,"
-                      "%d.%02d,%d\n" 
-                    , currentTime / 1000
-                    , ip(substrateController.temperature), fp(substrateController.temperature), int(substrateController.cv * 100)
-                    , ip(sourceController.temperature), fp(sourceController.temperature), int(sourceController.cv * 100));
-
-                if(DEBUG_ENABLED) {
-                    // Some code repetition
-                    pos += snprintf(serialBuf + pos, serialBufLen - pos
-                        , "TRACE1(SUBSTR): ERR=%3d P=%3d D=%3d "
-                          "uCV=%3d CV=%3d T=%3d\n"
-                        , ip(substrateController.error), int(substrateController.p * 100), int(substrateController.d * 100)
-                        , int(substrateController.uncappedCV * 100), int(substrateController.cv * 100), ip(substrateController.temperature));
-                            
-                    pos += snprintf(serialBuf + pos, serialBufLen - pos
-                        , "TRACE2(SOURCE): ERR=%3d P=%3d D=%3d "
-                          "uCV=%3d CV=%3d T=%3d\n"
-                        , ip(sourceController.error), int(sourceController.p * 100), int(sourceController.d * 100)
-                        , int(sourceController.uncappedCV * 100), int(sourceController.cv * 100), ip(sourceController.temperature));
-                    
-                    pos += snprintf(serialBuf + pos, serialBufLen - pos
-                        , "TIMER: started=%d, ended=%d, started_at=%lu"
-                        , int(depositionStarted), int(depositionEnded), depositionStartTime);
-                }
-
-                // Try to send all the info in one buffer to avoid flickering in receiving terminal
-                Serial.println(serialBuf);
+                sendData();
             }
         }
 
@@ -219,13 +215,55 @@ public:
             sourceController.pollRelay(currentTime - relayCycleStart);
         }
     }
+
+    /**
+     * @brief Send full reactor data to the serial port
+     */
+    void sendData() {
+        int pos = snprintf(serialBuf, serialBufLen
+            , "INFO: %lu,"
+                "%d.%02d,%d,"
+                "%d.%02d,%d\n" 
+            , currentTime / 1000
+            , ip(substrateController.temperature), fp(substrateController.temperature), int(substrateController.cv * 100)
+            , ip(sourceController.temperature), fp(sourceController.temperature), int(sourceController.cv * 100));
+
+        // Some code repetition
+        pos += snprintf(serialBuf + pos, serialBufLen - pos
+            , "TRACE1(SUBSTR): ERR=%3d P=%3d D=%3d "
+                "uCV=%3d CV=%3d T=%3d\n"
+            , ip(substrateController.error), int(substrateController.p * 100), int(substrateController.d * 100)
+            , int(substrateController.uncappedCV * 100), int(substrateController.cv * 100), ip(substrateController.temperature));
+                
+        pos += snprintf(serialBuf + pos, serialBufLen - pos
+            , "TRACE2(SOURCE): ERR=%3d P=%3d D=%3d "
+                "uCV=%3d CV=%3d T=%3d\n\n"
+            , ip(sourceController.error), int(sourceController.p * 100), int(sourceController.d * 100)
+            , int(sourceController.uncappedCV * 100), int(sourceController.cv * 100), ip(sourceController.temperature));
+
+        // Try to send all the info in one buffer to avoid flickering in receiving terminal
+        assert(pos < serialBufLen);
+        Serial.write(serialBuf, pos);
+        Serial.flush(); 
+    }
+
+    // #################### VARIABLES ####################
+private:
+    Controller substrateController {2, 3,  4,  5,  6, 22, 23, SUBSTRATE_POWER_LIMIT, SUBSTRATE_TEMP, SUBSTRATE_TEMP_OFFSET, SUBSTRATE_KP, SUBSTRATE_KD}; // Substrate
+    Controller sourceController    {8, 9, 10, 11, 12, 24, 25, SOURCE_POWER_LIMIT,    SOURCE_TEMP,    SOURCE_TEMP_OFFSET,    SOURCE_KP,    SOURCE_KD   }; // Source
+
+    static const size_t serialBufLen = 2048;
+    char serialBuf[serialBufLen];
+
+    unsigned long relayPollTime = 0;
+    unsigned long nextReadTime = 0;
+    unsigned long relayCycleStart = 0;
 };
 
 Reactor r{};
 
 void setup() {
     Serial.begin(9600);
-    Serial.println("\n\n\nStart");
     r.setup();
 }
 
